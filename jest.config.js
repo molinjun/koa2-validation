52526caa334bd7998db2f6b32502dd08cefef70d
莫林 <molin1226@163.com>
2019-11-10 02:18:55 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -27,7 +27,7 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ["/node_modules/", "test"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/test/"],
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
